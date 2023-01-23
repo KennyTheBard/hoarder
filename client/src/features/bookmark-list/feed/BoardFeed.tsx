0fd0948f544c9ad6098596a1c440eb4763b89ed1
KennyTheBard <octav_teculescu@yahoo.com>
2023-01-23 21:16:43 +0200

    Refactor BoardFeed to be more efficient

--- client/src/features/bookmark-list/feed/BoardFeed.tsx
+++ client/src/features/bookmark-list/feed/BoardFeed.tsx
@@ -1,5 +1,4 @@
 import { Grid, Stack } from '@mantine/core';
-import { Bookmark, WithId } from 'common';
 import { BookmarkCard } from '../../../components';
 import { useAppSelector } from '../../../redux/hooks';
 
@@ -10,19 +9,20 @@ export interface BoardFeedProps {
 
 export function BoardFeed(props: BoardFeedProps) {
 
-   const entriesPerColumn: WithId<Bookmark>[][] = Array.from(Array(props.columnCount).keys()).map(() => []);
    const bookmarks = useAppSelector((state) => state.bookmarkSlice.bookmarks);
-   bookmarks.forEach((entry: WithId<Bookmark>, index: number) => entriesPerColumn[index % props.columnCount].push(entry));
 
    return (
       <Grid columns={props.columnCount} justify="center" mb="100px">
-         {entriesPerColumn.map((entries, idx) =>
-            <Grid.Col key={idx} span={1}>
+         {Array.from(Array(props.columnCount).keys()).map((_, columnIndex) => (
+            <Grid.Col key={columnIndex} span={1}>
                <Stack>
-                  {entries.map(entry => <BookmarkCard key={entry.id} {...entry}/>)}
+                  {bookmarks
+                     .filter((_, entryIndex) => entryIndex % props.columnCount === columnIndex)
+                     .map(entry => <BookmarkCard key={entry.id} {...entry} />)
+                  }
                </Stack>
             </Grid.Col>
-         )}
+         ))}
       </Grid>
    );
 }
